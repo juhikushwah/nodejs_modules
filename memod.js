4f0a08ecd365c1cbd341437d421d5d6c88a4ac4c
juhi.kushwah <juhi.kushwah@ionidea.com>
2021-01-20 23:47:20 -0800

    Added synchronous and asynchronous methods

--- memod.js
+++ memod.js
@@ -20,6 +20,14 @@ const fs = require('fs');
 const files = fs.readdirSync('./') //using Sync form here as it's easy to understand; will return files for current(./) folder.        
 console.log(files);
 
+// Asynchronous form of the above method
+
+fs.readdir('./', function(err, files){     //instead of './' , use something else to generate an error and see the o/p!
+   if(err) console.log('Error', err);
+   else console.log('Result', files);
+} )      
+ 
+
 module.exports.func1 = os;
 
 /* When using node with JS, our code is executed outside of a browser, or as some people say, on the server.
@@ -34,4 +42,9 @@ module.exports.func1 = os;
    Node process has a single thread. Eg. If you are using node to build the backend for your application, you might have several
    hundreds of thousands of clients connecting to that backend. If you keep that single thread busy, you won't be able to
    serve many clients, therefore, it is always advisable to use ASYNC methods.
+
+   All these async methods take a function as their last argument, which node calls when that async operation completes(callback).
+
+   In order to work with files and directories, require the 'fs' module first, and then you can use one or more methods defined
+   in this module. Note, all these methods comes in pair(async and sync). Preferred:async methods.
  */
